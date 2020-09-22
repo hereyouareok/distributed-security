0eb031424984da6d741cd5312bc0d1f8c3495612
we <843705011@qq.com>
2020-09-22 15:20:51 +0800

    资源服务配置完成

--- distributed‐security‐order/src/main/java/com/it/security/distributed/OrderServer.java
+++ distributed‐security‐order/src/main/java/com/it/security/distributed/OrderServer.java
@@ -3,8 +3,6 @@ package com.it.security.distributed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author YanQin
@@ -14,8 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
-/*@EnableHystrix
-@EnableFeignClients(basePackages = {"com.it.security.distributed.order"})*/
 public class OrderServer {
     public static void main(String[] args) {
         SpringApplication.run(OrderServer.class, args);
